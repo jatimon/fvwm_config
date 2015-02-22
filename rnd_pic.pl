44b5ac8558cec2b059a313d90078368fa1d14ead
John Timon <john.a.timon@gmail.com>
2015-02-22 08:50:20 -0800

    updated config, moved to python for some scripts

--- rnd_pic.pl
+++ rnd_pic.pl
@@ -2,20 +2,24 @@
 
 use strict;
 
-	my($back_dir)="/export/home/53759/back";
+	#my($back_dir)="/export/home/53759/back";
+	my($back_dir)="/usr/share/backgrounds/linuxmint-rebecca";
 
 	show_pic($back_dir, $ARGV[1]) if ($ARGV[1]);
 
 	srand(time ^ $$ ^ unpack "%L*", `ps -ef | gzip`);
 
 	opendir DIR, $back_dir || die "unable to open background image directory\n";
-	my(@files)=grep{ /^\w+/ && !/^\.+/ && !/nsfw/ && -f "$back_dir/$_" } readdir(DIR);
+	my(@files)=grep{ /^\w+/ && !/^\.+/ && -f "$back_dir/$_" } readdir(DIR);
 
 	closedir DIR;
+
+	print "@files\n";
 
 	# now randomly choose one.
 	
 	#my($pic)= $files[rand @files];
+	print "$files[rand @files]";
 	show_pic($back_dir, $files[rand @files]);
 
 sub show_pic {
@@ -26,13 +30,13 @@ sub show_pic {
 
 	# if it says asis dont stretch it.
 	if ($pic =~ /asis/i) {
-		$command= "/usr/local/bin/xv -root -quit -rmode 5 $back_dir/$pic";
+		$command= "Esetroot -root -quit -rmode 5 $back_dir/$pic";
 	}
 	elsif ($pic =~ /tile/i) {
-		$command= "/usr/local/bin/xv -root -quit -rmode 1 $back_dir/$pic";
+		$command= "Esetroot -root -quit -rmode 1 $back_dir/$pic";
 	}
 	else {
-		$command="/usr/local/bin/xv -root -quit -max $back_dir/$pic";
+		$command="Esetroot  $back_dir/$pic";
 	}
 
 	if ($ARGV[0]) {
