9bbc58250068d63a21c803a3bd90c42d906132e8
John Timon <john.a.timon@gmail.com>
2016-05-22 12:17:14 -0700

    Latest

--- scripts/background_builder.py
+++ scripts/background_builder.py
@@ -1,28 +1,48 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-from os import walk, system
+import os
 import magic
-from collections import namedtuple
 
 def main():
     mypath="/usr/share/backgrounds"
-    a=[]
-    Image=namedtuple('Image', ['name', 'path', 'fqp'])
-    for (dirpath, dirnames, files) in walk(mypath):
-        a+=[ Image(path=dirpath, name=file, fqp=dirpath+'/'+file) for file in files if "jpg" in file]
-
     mymagic = magic.open(magic.MAGIC_MIME_TYPE)
     mymagic.load()
+    a = []
+    for (dirpath, dirnames, files) in os.walk(mypath):
+        for filename in files:
+            pic = '/'.join([dirpath, filename])
+            if 'image' in mymagic.file(pic):
+                a.append(pic)                               
+
     
-    f=open('/home/jtimon/.fvwm/WallpaperMenu','w')
+    home = os.environ['HOME']
+    f = open('{}/.fvwm/WallpaperMenu'.format(home),'w')
     for image in a:
-        if "image" in mymagic.file(image.fqp):
-            cmd="convert {0.fqp} -thumbnail 50x50 $HOME/.fvwm/temp_dir/{0.name}.png".format(image)
-            system(cmd)
-            menu="AddToMenu BackgroundMenu \"{0.name}\"%/home/jtimon/.fvwm/temp_dir/{0.name}.png% Function SetDefaultBackground {0.fqp}".format(image)
-            print(menu, file=f)
+        basename = os.path.basename(image)
+        if not os.path.isfile(
+            "{}/.fvwm/temp_dir/{}.png".format(home, basename.split('.')[0])
+        ):
+            cmd = (
+                    "convert {} -thumbnail 50x50 "
+                    "$HOME/.fvwm/temp_dir/{}.png".format(
+                        image,
+                        basename.split('.')[0]
+                    )
+            ) 
+            print(cmd)
+            os.system(cmd)
+
+        menu = (
+              'AddToMenu BackgroundMenu "{}"'
+              '"%/home/jtimon/.fvwm/temp_dir/{}.png%" '
+              'Function SetDefaultBackground {}'.format(
+                  basename,
+                  basename.split('.')[0],
+                  image
+                ))
+        print(menu, file=f)
     f.close()
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
